ec240150e3c9a34bd291e7148a59037a82fb80a6
Barto Molina <bartomolina@gmail.com>
2023-03-18 11:27:54 +0100

    Layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import "../styles/globals.css";
+import Head from "next/head";
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,8 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
+      <Head>
+        <link
+          rel="icon"
+          href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>◼️</text></svg>"
+        ></link>
+      </Head>
+      <body className="bg-gray-50">
         <Main />
         <NextScript />
       </body>
